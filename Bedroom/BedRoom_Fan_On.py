2ad05cff439635b40596dcdb9c8ccc4b05298d43
sandrac <schoysm@gmail.com>
2023-07-05 17:17:24 +0800

    PROGRESS!!!!!!!

--- Bedroom/BedRoom_Fan_On.py
+++ Bedroom/BedRoom_Fan_On.py
@@ -5,12 +5,12 @@ import time
 mqtt_broker = "192.168.1.3"
 mqtt_username = "admin"
 mqtt_pwd = "mqttbroker"
-mqtt_topic = "Doorway/MotionSensor"
+mqtt_topic = "BedRoom/Fan"
 
 
 publish.single(mqtt_topic, 
-                payload="Off", 
+                payload="On", 
                 retain=True,
                 hostname=mqtt_broker,
                 auth={"username": mqtt_username, "password": mqtt_pwd})
-print("MotionSensor Off")
+print("Fan On")
--- Bedroom/BedRoom_Light_Off.py
+++ Bedroom/BedRoom_Light_Off.py
@@ -5,7 +5,7 @@ import time
 mqtt_broker = "192.168.1.3"
 mqtt_username = "admin"
 mqtt_pwd = "mqttbroker"
-mqtt_topic = "Doorway/MotionSensor"
+mqtt_topic = "LivingRoom/Light"
 
 
 publish.single(mqtt_topic, 
@@ -13,4 +13,4 @@ publish.single(mqtt_topic,
                 retain=True,
                 hostname=mqtt_broker,
                 auth={"username": mqtt_username, "password": mqtt_pwd})
-print("MotionSensor Off")
+print("Light Off")
