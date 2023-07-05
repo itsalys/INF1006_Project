77e8379001317a4eabca0cd0cc6c18ed1fff3d16
2202528 <111504470+2202528@users.noreply.github.com>
2023-07-05 19:14:21 +0800

    added html pages and navigation in index py

--- WebApplication/index.py
+++ WebApplication/index.py
@@ -14,5 +14,21 @@ def index():
 def analytics():
     return render_template('index.html')
 
+@app.route('/doorway')
+def doorway():
+    return render_template('doorway.html')
+
+@app.route('/livingroom')
+def livingroom():
+    return render_template('livingroom.html')
+
+@app.route('/babyroom')
+def babyroom():
+    return render_template('babyroom.html')
+
+@app.route('/bedroom')
+def bedroom():
+    return render_template('bedroom.html')
+
 if __name__=="__main__":
     app.run(debug=True, host="0.0.0.0", port=5050)
