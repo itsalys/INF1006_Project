0085a7ed3653f3b46f99ababaa06834dfd7f01d4
claris toh <claristoh0911@gmail.com>
2023-07-07 17:37:11 +0800

    doorway camera

--- Living Room/LivingRoom_Light_On.py
+++ Living Room/LivingRoom_Light_On.py
@@ -1,8 +1,9 @@
 # pip3 install paho-mqtt
 import paho.mqtt.publish as publish
 import time
+from gpiozero import LED
 
-mqtt_broker = "192.168.1.2"
+mqtt_broker = "192.168.1.3"
 mqtt_username = "admin"
 mqtt_pwd = "mqttbroker"
 mqtt_topic = "LivingRoom/Light"
@@ -14,3 +15,5 @@ publish.single(mqtt_topic,
                 hostname=mqtt_broker,
                 auth={"username": mqtt_username, "password": mqtt_pwd})
 print("Light On")
+led = LED()
+led.on()
