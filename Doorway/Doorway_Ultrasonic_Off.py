760348bcbfe135e7db4a55f20a19b9a879cd5318
Sandra C <schoysm@gmail.com>
2023-07-08 02:33:12 +0800

    add/move ultrasonic code to doorway

---
+++ Doorway/Doorway_Ultrasonic_Off.py
@@ -0,0 +1,16 @@
+# pip3 install paho-mqtt
+import paho.mqtt.publish as publish
+import time
+
+mqtt_broker = "192.168.1.2"
+mqtt_username = "admin"
+mqtt_pwd = "mqttbroker"
+mqtt_topic = "Doorway/Ultrasonic"
+
+
+publish.single(mqtt_topic, 
+                payload="Off", 
+                retain=True,
+                hostname=mqtt_broker,
+                auth={"username": mqtt_username, "password": mqtt_pwd})
+print("Ultrasonic Sensor Off")
