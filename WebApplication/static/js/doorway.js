43c8795c16b8c685550128f320be3fe5fd168e2c
2202528 <111504470+2202528@users.noreply.github.com>
2023-07-14 17:33:08 +0800

    push

---
+++ WebApplication/static/js/doorway.js
@@ -0,0 +1,42 @@
+function createNotifCard(){
+
+    const notifDiv = document.createElement("div");
+    notifDiv.className = "secnotifdiv"
+
+    const notifBdy = document.createElement("div");
+    notifBdy.className = "secnotifbdy"
+
+    const notifLeftDiv = document.createElement("div");
+    notifLeftDiv.className = "secnotifleftdiv"
+
+    const notifTxtDiv = document.createElement("div");
+    notifBdy.className = "secnotiftxtdiv"
+
+    const notifTxt = document.createElement("p");
+    notifTxt.className = "secnotiftxt"
+
+    const notifTimeDiv = document.createElement("div");
+    notifBdy.className = "secnotifbdy"
+
+    const notifRightDiv = document.createElement("div");
+    notifRightDiv.className = "secnotifrightdiv"
+
+    const notifCloseDiv = document.createElement("div");
+    notifCloseDiv.className = "secnotifclosediv"
+
+    
+
+    const notifBdy = document.createElement("div");
+    notifBdy.className = "secnotifbdy"
+
+    const notifBdy = document.createElement("div");
+    notifBdy.className = "secnotifbdy"
+
+    const notifBdy = document.createElement("div");
+    notifBdy.className = "secnotifbdy"
+
+    const notifBdy = document.createElement("div");
+    notifBdy.className = "secnotifbdy"
+}
+
+$(document).ready(function() {});
