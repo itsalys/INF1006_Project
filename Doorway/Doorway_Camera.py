17f7758da24c98404dbf4f9997137aa76e2f8b48
claris toh <claristoh0911@gmail.com>
2023-07-14 15:27:12 +0800

    face detect publish mqtt

--- Doorway/Doorway_Camera.py
+++ Doorway/Doorway_Camera.py
@@ -3,6 +3,7 @@ from time import sleep
 import paho.mqtt.client as mqtt
 import base64
 import paho.mqtt.publish as publish
+import cv2
 
 # camera = PiCamera()
 # camera.start_preview()
@@ -11,14 +12,62 @@ import paho.mqtt.publish as publish
 # camera.stop_recording()
 # camera.stop_preview()
 
-
 mqtt_broker = "192.168.1.2"
 mqtt_username = "admin"
 mqtt_pwd = "mqttbroker"
 mqtt_topic = "Doorway/Camera"
 
+
+
+import cv2
+
+# Set up the face detection classifier
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+
+# Initialize the video capture
+video_capture = cv2.VideoCapture(0)
+
+# Continuously capture frames from the camera
+while True:
+    # Read a frame from the video capture
+    ret, frame = video_capture.read()
+
+    # Convert the captured frame to grayscale for face detection
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # Detect faces in the grayscale frame
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+
+    # Check if any faces are detected
+    if len(faces) > 0:
+        # Draw rectangles around the detected faces
+        for (x, y, w, h) in faces:
+            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+        # Display the frame with face detections
+        cv2.imshow('Face Detection', frame)
+
+        # Save the image if a face is detected
+        cv2.imwrite('face_detected.jpg', frame)
+        print("Image saved!")
+
+        break
+
+    # Display the frame without face detections
+    cv2.imshow('Face Detection', frame)
+
+    # Break the loop if 'q' is pressed
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# Release the video capture resources
+video_capture.release()
+
+# Destroy all OpenCV windows
+cv2.destroyAllWindows()
+
 # Read the image file
-image_path = 'C:/Users/clari/Desktop/yelan.jpg'
+image_path = 'face_detected.jpg'
 with open(image_path, "rb") as image_file:
     encoded_image = base64.b64encode(image_file.read()).decode('utf-8')
 
@@ -36,7 +85,7 @@ client.connect(mqtt_broker, 1883)
 
 # Publish the image to the MQTT broker
 client.publish(mqtt_topic, encoded_image)
-print("here")
+print("encode: ", encoded_image)
 
 # Disconnect from the MQTT broker
 client.disconnect()
