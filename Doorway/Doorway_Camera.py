d3b9acabd7d9a64daccd45af439324aa9a8e0a63
claris toh <claristoh0911@gmail.com>
2023-07-14 15:19:14 +0800

    mqtt camera

--- Doorway/Doorway_Camera.py
+++ Doorway/Doorway_Camera.py
@@ -1,9 +1,42 @@
-from picamera import PiCamera
+#from picamera import PiCamera
 from time import sleep
+import paho.mqtt.client as mqtt
+import base64
+import paho.mqtt.publish as publish
 
-camera = PiCamera()
-camera.start_preview()
-camera.start_recording('/home/pi/Desktop/video.h264')
-sleep(5)
-camera.stop_recording()
-camera.stop_preview()
+# camera = PiCamera()
+# camera.start_preview()
+# camera.start_recording('/home/pi/Desktop/video.h264')
+# sleep(5)
+# camera.stop_recording()
+# camera.stop_preview()
+
+
+mqtt_broker = "192.168.1.2"
+mqtt_username = "admin"
+mqtt_pwd = "mqttbroker"
+mqtt_topic = "Doorway/Camera"
+
+# Read the image file
+image_path = 'C:/Users/clari/Desktop/yelan.jpg'
+with open(image_path, "rb") as image_file:
+    encoded_image = base64.b64encode(image_file.read()).decode('utf-8')
+
+# Create MQTT client
+client = mqtt.Client()
+
+publish.single(mqtt_topic, 
+                payload=encoded_image, 
+                retain=True,
+                hostname=mqtt_broker,
+                auth={"username": mqtt_username, "password": mqtt_pwd})
+
+# Connect to the MQTT broker
+client.connect(mqtt_broker, 1883) 
+
+# Publish the image to the MQTT broker
+client.publish(mqtt_topic, encoded_image)
+print("here")
+
+# Disconnect from the MQTT broker
+client.disconnect()
